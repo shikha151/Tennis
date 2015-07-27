1707f001fd46319323e198ae2cea3128cc6df26c
YashuAmaresh <yashaswini.amaresh@gmail.com>
2015-07-27 11:28:53 +0530

    Formatting TennisClient.java

--- JavaApplication4/src/tennis/TennisClient.java
+++ JavaApplication4/src/tennis/TennisClient.java
@@ -13,18 +13,14 @@ public class TennisClient {
     
     public static void main(String[] args) {
         System.out.println("Give the input string");
-        try{
+        try {
             BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
             String s = bufferRead.readLine();
             TennisGame game = new TennisGame(s);
-
-            //System.out.println(s);
         }
         catch(IOException e)
         {
             e.printStackTrace();
         }
-
-
     }
 }
