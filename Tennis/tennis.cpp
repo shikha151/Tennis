d0b71e41a2e466255fe602b614369e3cc43434c9
unknown <test@L-MAA-00441244.paypalcorp.com>
2015-07-27 10:18:25 +0530

    First Commit

---
+++ Tennis/tennis.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include,using namespace std;
+
+const int scoreProgression[4] = { 0, 15, 30, 40 };
+
+class player{
+	int currentScore;
+
+public:
+	player(){
+		currentScore = 0;
+	}
+};
+
+int main(){
+	player A, B;
+	
+}
