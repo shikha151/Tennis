655d07093cfcbbcf91785f00b375092fe62285a4
DhanyaHN <dhanyabharadwaj03@gmail.com>
2015-07-27 13:18:29 +0530

    computation method added without Handling deuce

--- JavaApplication4/src/tennis/TennisClient.java
+++ JavaApplication4/src/tennis/TennisClient.java
@@ -5,6 +5,10 @@
  */
 package tennis;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author test
@@ -16,15 +20,93 @@ public class TennisClient {
         try{
             BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
             String s = bufferRead.readLine();
-            TennisGame game = new TennisGame(s);
+            ScoreBoard scoreboard = new ScoreBoard();
 
-            //System.out.println(s);
+
+            
+            for( int i=0;i<s.length();i++){
+                if ( scoreboard.updateScore(s.charAt(i)) ){
+                    break;
+                }
+                scoreboard.displayScore();
+            }
         }
         catch(IOException e)
         {
             e.printStackTrace();
         }
+    }
+}
+class ScoreBoard {
+    private String p1;
+    private String p2;
+    String [] scores = {"0" , "15", "30", "40", "A", "W" };
+    int p1Index;
+    int p2Index;
+    
+    
+    public ScoreBoard( ) {
+        this.p1 = "0";
+        this.p2  = "0";
+    }
 
+    public void setP1Score( String p1) {
+        this.p1 = p1;
+    }
+    public void setP2Score( String p2) {
+        this.p2 = p2;
+    }
 
+    public boolean updateScore( char player ) {
+        boolean gameDone = false;
+        System.out.print(player + "   " +this.p1Index + "    " + this.p2Index + "    ");
+        if ( player == 'D' ) {
+            
+            if ( bothAtAdvantage() ) {
+                this.p1Index = 3;
+                this.p2Index = 3;
+                
+            }
+            else if ( this.p1Index == 5 && this.p1Index - this.p2Index == 2 || scores[this.p1Index] == "40" && this.p2Index < this.p1Index ) {
+                System.out.println("D wins");
+                gameDone = true;
+            }
+            else {
+                p1Index++;
+                if ( this.p1Index == 5 && this.p1Index - this.p2Index == 2 ) {
+                    gameDone = true;
+                    System.out.println(" D Wins");
+                }
+            }
+        }
+        else if ( player == 'F' ) {
+            
+            if ( bothAtAdvantage() ) {
+                this.p1Index = 3;
+                this.p2Index = 3;
+
+            }
+            else if ( this.p2Index == 5 && this.p2Index - this.p1Index == 2 || scores[this.p2Index] == "40" && this.p1Index < this.p2Index ) {
+                System.out.println("F wins");
+                gameDone = true;
+            }
+            else {
+                p2Index++;
+                if ( this.p2Index == 5 && this.p2Index - this.p1Index == 2 ) {
+                    gameDone = true;
+                    System.out.println("F Wins");
+                }
+            }
+        }
+        
+        setP1Score(scores[p1Index]);
+        setP2Score(scores[p2Index]);
+        return gameDone;
+    }
+    private boolean bothAtAdvantage() {
+        return this.p1Index == 4 && this.p2Index == 4;   
+    }
+    public void displayScore() {
+        System.out.println(this.p1 + "   " + this.p2);
     }
 }
